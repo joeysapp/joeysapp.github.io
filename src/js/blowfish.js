cd22c828f8bb1912c51ef4368777109ac11fbe26
Joseph Sapp <sapp.joey@tamu.edu>
2018-08-09 17:23:25 -0500

    stop slowdowns

--- src/js/blowfish.js
+++ src/js/blowfish.js
@@ -1,36 +1,29 @@
 $(document).ready(e => {
-  var char = 0;
-  var switched = false;
-  setInterval(() => {
-    for (var i = 0; i < Math.random()*5; i++){
-      $('#boartusk').prepend(String.fromCharCode(97 + char));
+  var choices = ['hobbit', 'fellowship', 'twotowers', 'returnoftheking', 'silmarillion'];
+  var book = choices[Math.floor(Math.random() * choices.length)];
+  $.get({
+    url: `src/data/${book}.txt`
+  }, (res, status) => {
+    if (status == 'success'){
+      var book = res.split(/\r?\n/);
+      var line_idx = 0;
+      var char_idx = 0;
+      setInterval(() => {
+        $('#boartusk').append(book[line_idx][char_idx]);
+        if (char_idx >= book[line_idx].length){
+           $('#boartusk').append(' ');
+          char_idx = 0;
+          line_idx++;
+        } else {
+          char_idx++;
+        }
+        var html = $('#boartusk').html();
+        var len = html.length;
+
+      }, 25);
+    } else {
+      console.log('[ERROR]', status);
     }
-    // $('#boartusk').scrollTop($('#boartusk').prop('scrollHeight'));
-    if (Math.random() > 0.1){
-      char += 1;
-    }
-    if (char > 1600 && !switched){
-      char = 2000;
-      switched = true;
-    }
-  }, 1);
+  });
+
 });
-
-
-// var s = function(p){
-//   p.setup = function(){
-//     p.createCanvas(p.windowWidth, p.windowHeight);
-//   };
-
-//   p.draw = function(){
-//     p.strokeWeight(p.random(10));
-//     p.point(p.random(p.windowWidth), p.random(p.windowHeight));
-//   };
-
-//   p.canvasResized = function(){
-//     p.resizeCanvas(p.windowWidth, p.windowHeight);
-//   };
-
-// };
-
-// var f = new p5(s, 'boar-tusk');
