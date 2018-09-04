50a4d68574dc35fc0297db1364b9bb8aec017518
Joseph Sapp <sapp.joey@tamu.edu>
2018-09-04 16:37:49 -0500

    lotr is done

--- src/js/blowfish.js
+++ src/js/blowfish.js
@@ -1,29 +1,3 @@
 $(document).ready(e => {
-  var choices = ['hobbit', 'fellowship', 'twotowers', 'returnoftheking', 'silmarillion'];
-  var book = choices[Math.floor(Math.random() * choices.length)];
-  $.get({
-    url: `src/data/${book}.txt`
-  }, (res, status) => {
-    if (status == 'success'){
-      var book = res.split(/\r?\n/);
-      var line_idx = 0;
-      var char_idx = 0;
-      setInterval(() => {
-        $('#boartusk').append(book[line_idx][char_idx]);
-        if (char_idx >= book[line_idx].length){
-           $('#boartusk').append(line_idx <= 5 ? '<br>' : ' ');
-          char_idx = 0;
-          line_idx++;
-        } else {
-          char_idx++;
-        }
-        var html = $('#boartusk').html();
-        var len = html.length;
-
-      }, 25);
-    } else {
-      console.log('[ERROR]', status);
-    }
-  });
 
 });
