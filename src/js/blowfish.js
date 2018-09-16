415cd95aaf007882ee98e7e2c178c8c6d907f4a3
Joseph Sapp <sapp.joey@tamu.edu>
2018-09-16 15:40:36 -0500

    I need to code more

--- src/js/blowfish.js
+++ src/js/blowfish.js
@@ -1,3 +1,31 @@
 $(document).ready(e => {
 
+  let burp;
+  let canvas;
+  let time;
+  let hour;
+  let minute;
+  let second;
+  let millisecond;
+
+  var s = function(p){
+
+    p.setup = function(){
+      burp = p.createDiv('<div>HI</div>');
+      canvas = p.createCanvas(0, 0);
+    };
+
+   };
+   var f = new p5(s, 'container'); 
+
+
+  setInterval(e => {
+    // console.log('hi');
+    time = new Date();
+    hour = time.getHours();
+    minute = time.getMinutes();
+    second = time.getSeconds();
+    millisecond = time.getMilliseconds();
+    burp.html(`${hour}:${minute}:${second}:${millisecond}`);
+  }, 1);
 });
