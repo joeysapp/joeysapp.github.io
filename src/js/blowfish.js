4803f216110d80a813cecc9b03cb29a23295fb4b
Joseph Sapp <sapp.joey@tamu.edu>
2018-09-20 19:39:56 -0500

    spooky user agent

--- src/js/blowfish.js
+++ src/js/blowfish.js
@@ -1,31 +1,16 @@
 $(document).ready(e => {
 
-  let burp;
+  let div;
   let canvas;
-  let time;
-  let hour;
-  let minute;
-  let second;
-  let millisecond;
+
 
   var s = function(p){
 
     p.setup = function(){
-      burp = p.createDiv('<div>HI</div>');
       canvas = p.createCanvas(0, 0);
+      div = p.createDiv(`${navigator.userAgent}`);
     };
 
    };
    var f = new p5(s, 'container'); 
-
-
-  setInterval(e => {
-    // console.log('hi');
-    time = new Date();
-    hour = time.getHours();
-    minute = time.getMinutes();
-    second = time.getSeconds();
-    millisecond = time.getMilliseconds();
-    burp.html(`${hour}:${minute}:${second}:${millisecond}`);
-  }, 1);
 });
