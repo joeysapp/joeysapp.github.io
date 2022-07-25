efff9d73c1e995d06d0b733bedffc1f233abfb04
joeysapp <sapp.joey@gmail.com>
2022-07-25 18:26:11 -0500

    visible on phones now

--- src/js/m.js
+++ src/js/m.js
@@ -2,6 +2,8 @@
 // import perlin from './perlin.js';
 // import simplex from './simplex.js';
 
+let seed = Math.floor(Math.random()*65536);
+
 function Vector(x, y, z, args) {
   this.x = x;
   this.y = y;
@@ -31,9 +33,12 @@ function Vector(x, y, z, args) {
 }
 
 function Node() {
-  this.pos = new Vector(0, 0, 0);
+  this.pos = new Vector(42, 0, 0);
   this.vel = new Vector(0, 0, 0);
   this.acc = new Vector(0, 0, 0);
+  this.xdiv = 32;
+  this.ydiv = 32;
+  this.zdiv = 32;
   this.mass = 1.0;
 
   Node.prototype.applyForce = function(force) {
@@ -44,7 +49,7 @@ function Node() {
 
   Node.prototype.update = function() {
     this.vel.add(this.acc);
-    this.vel.mult(0.5);
+    this.vel.mult(0.93);
     this.pos.add(this.vel);
     this.acc.mult(0);
   }
@@ -53,27 +58,36 @@ function Node() {
 const m = () => {
   const ua = navigator.userAgent;
   let text = '';
-  let height = Math.floor((window.innerHeight)/5);
-  let width =  Math.floor((window.innerWidth)/9);
+  let height = Math.floor((window.innerHeight)/6);
+  let width =  Math.floor((window.innerWidth)/2.4);
   
   const head = new Node();
   const gravity = new Vector(-1, 0, 0);
 
-  let seed = Math.floor(Math.random()*65536);
   let simplex = new SimplexNoise(seed);
   console.log(head);
 
   setInterval(() => {
-    const ndiv = 64;
-    const { x, y, z } = head.pos;
-    let n = simplex.noise3D((x+0)/ndiv, (y+0)/ndiv, (z+0)/ndiv);
+    seed = Math.floor(Math.random()*65536);
 
-    n = map(n, -1, 1, 0, Math.PI * 2.0);
-    
-    let r = 0.3;
-    let nx = r * Math.cos(n) * Math.sin(n);
-    let ny = r * Math.sin(n) * Math.sin(n);
-    let nz = r * Math.cos(n);
+    head.pos = new Vector(Math.random()*65536, Math.random()*65536, Math.random()*65536);
+    head.xdiv = 16 + Math.random()*128;
+    head.ydiv = 16 + Math.random()*128;
+    head.zdiv = 16 + Math.random()*128;
+  }, 2500);
+
+  setInterval(() => {
+    const { x, y, z } = head.pos;
+    const { xdiv, ydiv, zdiv } = head;
+    let noise = simplex.noise3D((x+0)/xdiv, (y+0)/ydiv, (z+0)/zdiv);
+
+    let theta = map(noise, -1, 1, 0, Math.PI);
+    let phi = map(noise, -1, 1, 0, Math.PI * 2.0);
+
+    let r = 0.2;
+    let nx = r * Math.cos(theta) * Math.sin(phi);
+    let ny = r * Math.sin(theta) * Math.sin(phi);
+    let nz = r * Math.cos(theta);
 
     const simplexForce = new Vector(nx, ny, nz);
     head.applyForce(simplexForce);
@@ -83,19 +97,20 @@ const m = () => {
     // console.log(head.vel);
 
     let string = '';
-    for (let wx = -width/2; wx < width/2; wx += 1) {
+    for (let wx = -width/2; wx <= width/2; wx += 1) {
       for (let wy = -height/2; wy <= height/2; wy += 1) {
         let char = '';
-        let n = simplex.noise3D((x+wx)/ndiv, (y+wy)/ndiv, (z+0)/ndiv);
+        let n = simplex.noise3D((x+wx)/xdiv, (y+wy)/ydiv, (z+0)/zdiv);
         // n = map(n, -1, 1, 161, 1000);
-        n = map(n, -1, 1, 700, 900);
+        // n = map(n, -1, 1, 700, 900);
+        n = map(n, -1, 1, 600, 800);
         char = Math.floor(n);
         string += String.fromCharCode(parseInt(n, 10));
       }
       string += '\n';
     }
     document.getElementById('container').innerHTML = string;
-  }, 10);
+  }, 5);
 };
 
 const map = (v, vlow, vhigh, low, high) => {
